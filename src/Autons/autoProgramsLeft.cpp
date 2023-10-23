739d43ea13d06b338272851d183ee385a9068ae1
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-10-23 12:19:08 -0400

    test

---
+++ src/Autons/autoProgramsLeft.cpp
@@ -0,0 +1,44 @@
+#include "autons.hpp"
+#include "display.hpp"
+
+//#include "okapi/api.hpp"
+
+using namespace okapi;
+
+void leftAuton(void) {
+	pros::screen::set_pen(COLOR_BLUE);
+    pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Blue Flag");
+	/*//lift.moveAbsolute(50, 200);
+	drive->setMaxVelocity(200);
+	intake.moveVelocity(200);
+	drive->moveDistance(96_cm);
+	drive->setMaxVelocity(80);
+	drive->moveDistance(30_cm);
+	drive->setMaxVelocity(200);
+	drive->turnAngle(-1_deg);
+	drive->moveDistance(-120_cm);
+	align(-40);
+	drive->setMaxVelocity(30);
+	//drive->moveDistance(17_cm); // Somewhat Works
+	drive->moveDistance(15_cm);
+	pros::delay(200);
+	fireCatapult();
+	//drive->moveDistance(-10_cm);
+	drive->setMaxVelocity(150);
+	drive->turnAngle(97_deg);
+	drive->setMaxVelocity(200);
+	drive->moveDistance(130_cm);
+	drive->moveDistance(-75_cm);
+	intake.moveVelocity(-200);
+	drive->setMaxVelocity(150);
+	drive->turnAngle(90_deg);
+	drive->moveDistanceAsync(-50_cm);
+	pros::delay(200);
+	lift.moveAbsolute(550, 200);
+	pros::delay(1500);*/
+	
+	
+}
+
+
+
